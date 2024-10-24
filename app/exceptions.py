89b60f2bf2e18e0f7443c56c9fb0223f24c065a5
Alorian <slowpok19450509@gmail.com>
2024-10-24 13:46:18 +0200

    feat: add user dependencies

--- app/exceptions.py
+++ app/exceptions.py
@@ -17,7 +17,7 @@ UserAlreadyExistsException = HTTPException(status_code=status.HTTP_409_CONFLICT,
 PasswordMismatchException = HTTPException(status_code=status.HTTP_409_CONFLICT, detail="The passwords don't match!")
 
 IncorrectEmailOrPasswordException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                                                  detail="Wron email or password")
+                                                  detail="Wrong email or password")
 
 NoJwtException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                detail="The token is not valid")
